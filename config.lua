48c6795bb2c1cb95bf1e5041a2b8168c4981e62e
Enrique Mendoza <luis.e.1998@hotmail.com>
2022-12-19 09:49:54 -0700

    add colorizer config

--- config.lua
+++ config.lua
@@ -1,20 +1,21 @@
-reload "user.lsp"
-reload "user.bqf"
-reload "user.git"
-reload "user.goto-preview"
-reload "user.inlay-hints"
-reload "user.jay"
-reload "user.keymaps"
-reload "user.lsp_signature"
-reload "user.markdown-preview"
-reload "user.numb"
-reload "user.options"
-reload "user.plugins"
-reload "user.smoothie"
-reload "user.symbols-outline"
-reload "user.telescope-plugins"
-reload "user.todo-comments"
-reload "user.trouble"
-reload "user.ts-autotag"
-reload "user.ts-context"
-reload "user.zen-mode"
+reload("user.lsp")
+reload("user.bqf")
+reload("user.colorizer")
+reload("user.git")
+reload("user.goto-preview")
+reload("user.inlay-hints")
+reload("user.jay")
+reload("user.keymaps")
+reload("user.lsp_signature")
+reload("user.markdown-preview")
+reload("user.numb")
+reload("user.options")
+reload("user.plugins")
+reload("user.smoothie")
+reload("user.symbols-outline")
+reload("user.telescope-plugins")
+reload("user.todo-comments")
+reload("user.trouble")
+reload("user.ts-autotag")
+reload("user.ts-context")
+reload("user.zen-mode")
