14bd3f54b64a5af43b5420cee5a0445bb63ed6ae
Enrique Mendoza <luis.e.1998@hotmail.com>
2022-12-19 22:59:07 -0700

    update whichkey

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -7,3 +7,35 @@ lvim.builtin.which_key.mappings["G"] = {
 	-- Only set if you have telescope installed
 	r = { "<Cmd>lua require('goto-preview').goto_preview_references()<CR>", "References" },
 }
+
+-- For LSP Saga
+-- lvim.builtin.which_key.mappings["S"] = {
+-- 	name = "Saga",
+-- Lsp finder find the symbol definition implement reference
+-- if there is no implement it will hide
+-- when you use action in finder like open vsplit then you can
+-- use <C-t> to jump back
+-- f = { "<Cmd>Lspsaga lsp_finder<CR>", "Finder" },
+-- Code action
+-- a = { "<Cmd>Lspsaga code_action<CR>", "Code Action" },
+-- Rename
+-- r = { "<Cmd>Lspsaga rename<CR>", "Rename" },
+-- Peek Definition
+-- you can edit the definition file in this flaotwindow
+-- also support open/vsplit/etc operation check definition_action_keys
+-- support tagstack C-t jump back
+-- p = { "<Cmd>Lspsaga peek_definition<CR>", "Peek Definition" },
+-- Show line diagnostics
+-- s = { "<Cmd>Lspsaga show_line_diagnostics<CR>", "Show Line Diagnostics" },
+-- Show cursor diagnostics
+-- c = { "<Cmd>Lspsaga show_cursor_diagnostics<CR>", "Show Cursor Diagnostics" },
+-- Jump to error
+-- j = { "<Cmd>Lspsaga diagnostic_jump_prev<CR>", "Prev Diagnostics" },
+-- k = { "<Cmd>Lspsaga diagnostic_jump_next<CR>", "Next Diagnostics" },
+-- Outline
+-- o = { "<Cmd>LSoutlineToggle<CR>", "Outline" },
+-- Hover Doc
+-- K = { "<Cmd>Lspsaga hover_doc<CR>", "Hover Doc" },
+-- Lazygit in terminal
+-- l = { "<Cmd>Lspsaga open_floaterm lazygit<CR>", "Lazygit in Terminal" },
+-- }
